08e5dc507b089909d236d6f65919311967261e23
shivamfet <shivamfet@gmail.com>
2021-01-31 22:20:29 +0530

    Added the required Validation for Login functionality

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,9 +1,9 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import './Login.css';
-// import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
@@ -16,14 +16,42 @@ class Login extends Component {
     constructor() {
         super();
         this.state = {
-            username : "",
-            password : ""
+            username: "",
+            password: "",
+            usernameRequired: "dispNone",
+            passwordRequired: "dispNone",
+            isLoginSuccessful: "",
+            areCredentialsCorrect : ""
         }
+    }
+
+    inputUsernameHandler = (e) => {
+        this.setState({areCredentialsCorrect : ""})
+        this.setState({ usernameRequired: "dispNone" })
+        this.setState({username : e.target.value});
+        console.log(this.state.username);
+    }
+
+    inputPasswordHandler = (e) => {
+        this.setState({areCredentialsCorrect : ""})
+        this.setState({ passwordRequired: "dispNone" })
+        this.setState({password : e.target.value})
+        console.log(this.state.password);
     }
 
     loginClickHandler = () => {
         let username = "abc";
         let password = "abc";
+        
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+
+        if (this.state.username === username && this.state.password === password) {
+            console.log("Login is successful")
+            this.setState({ isLoginSuccessful: "true" });
+        } else {
+            this.setState({areCredentialsCorrect : "false"});
+        }
     }
 
     render() {
@@ -31,25 +59,37 @@ class Login extends Component {
             <div>
                 <Header />
                 <div className="card-container">
-                     <Card style={{ width: "18rem", padding : "48px 48px 48px 48px"}}>
-                         <CardContent>
+                    <Card style={{ width: "22rem", padding: "48px 48px 48px 48px" }}>
+                        <CardContent>
                             <Typography variant="headline" component="h2">
                                 LOGIN
                             </Typography>
-                            <br/>
-                             <FormControl required>
+                            <br />
+                            <FormControl required>
                                 <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" type="text" username={this.state.username}/>
+                                <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameHandler}/>
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                             </FormControl>
-                            <br/><br/>
+                            <br /><br />
                             <FormControl required>
                                 <InputLabel htmlFor="loginPassword">Password</InputLabel>
-                                <Input id="loginPassword" type="password" loginpassword={this.state.password}/>
+                                <Input id="loginPassword" type="password" loginPassword={this.state.password} onChange = {this.inputPasswordHandler}/>
+                                <FormHelperText className={this.state.passwordRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                             </FormControl>
-                            <br/><br/><br/>
+                            <br /><br /><br />
                             <Button variant="contained" color="primary" id="login_button" onClick={this.loginClickHandler}>LOGIN</Button>
-                         </CardContent>
-                    </Card> 
+                            <br /><br />
+                            {this.state.usernameRequired === "dispNone" && this.state.passwordRequired === "dispNone" && this.state.areCredentialsCorrect === "false" &&
+                                <FormControl>
+                                    <span className="red">Incorrect username and/or password</span>
+                                </FormControl>
+                            }
+                        </CardContent>
+                    </Card>
                 </div>
             </div>)
     }
