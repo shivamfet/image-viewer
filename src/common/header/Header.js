2114c0e71101100ee6b5bdb5744c40251abc039e
shivamfet <shivamfet@gmail.com>
2021-02-01 21:01:53 +0530

    Added search box

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,19 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
 import './Header.css';
 
 class Header extends Component {
 
     render() {
-        return(<div className="header">
-            Image Viewer
-        </div>);
+        return (
+            <div className="container">
+                <div className="header">Image Viewer</div>
+                <div className="searchBox">
+                    <SearchIcon style={{ color: "black" }} />
+                    <Input disableUnderline={true} placeholder="Search" />
+                </div>
+            </div>);
     }
 }
 
